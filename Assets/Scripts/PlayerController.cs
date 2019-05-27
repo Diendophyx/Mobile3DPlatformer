f23bcaef3afb0e537700a5946c2bec15f99eec68
Diendophyx <42424060+Diendophyx@users.noreply.github.com>
2019-05-27 16:32:03 +1000

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+    public Rigidbody rigid;
+    // Use this for initialization
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.velocity = new Vector3(3, rigid.velocity.y);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigid.velocity = new Vector2(rigid.velocity.x, 3);
+        }
+
+    }
+}
